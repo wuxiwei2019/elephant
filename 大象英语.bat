9a1df8e14779af432a1155d557cdc6ca2a4b9ad2
WuXiWei <85207476@qq.com>
2021-04-12 22:48:04 +0800

    frist-test-v1.0 add lib folder

--- 大象英语.bat
+++ 大象英语.bat
@@ -1,3 +1,4 @@
+@echo off
 set current_path=%cd%
 ::start chrome.exe --allow-file-access-from-files  --app=https://www.baidu.com
 start chrome.exe --allow-file-access-from-files  --app=file:///%current_path%/index.html
