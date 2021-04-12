eda19655106059991ff8c1d9cef01692e82a6c15
WuXiWei <85207476@qq.com>
2021-04-12 17:20:59 +0800

    frist-test-v1.0 add lib folder

---
+++ config/letterDB.js
@@ -0,0 +1,30 @@
+let letterDbs = {
+    "level1": [
+     "dog",
+     "big",
+     "deer",
+     "door",
+     "duck",
+     "pig",
+     "apple",
+     "hug",
+     "kiss",
+     "sleep",
+     "watch",
+     "sit",
+     "bye",
+     "tree",
+     "car",
+     "cat"
+     ],
+     "level2": [
+     "tree",
+     "car",
+     "yes",
+     "green"
+     ],
+     
+}
+let levelNameList = ["level1","level2"];
+
+ 
