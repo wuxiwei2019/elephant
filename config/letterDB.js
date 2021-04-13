525be82af56b8190161e56c8e4e1c7dffe8778a7
WuXiWei <85207476@qq.com>
2021-04-13 23:16:05 +0800

    frist-test-v1.0 modify dialog

--- config/letterDB.js
+++ config/letterDB.js
@@ -1,30 +1,48 @@
 let letterDbs = {
+    // 三年级英语上册73个
     "level1": [
-     "dog",
-     "big",
-     "deer",
-     "door",
-     "duck",
-     "pig",
-     "apple",
-     "hug",
-     "kiss",
-     "sleep",
-     "watch",
-     "sit",
-     "bye",
-     "tree",
-     "car",
-     "cat"
+     "pen","pencil","pencil-case","ruler","eraser","crayon","book","bag","sharpener","school",
+     "head","face","nose","mouth","eye"," ear","arm","finger","leg","foot",
+     "body","red","yellow","green","blue","purple","white"," black","orange","pink",
+     "brown","cat","dog","monkey","panda","rabbit","duck","pig"," bird","bear",
+     "elephant","mouse","squirrel","cake","bread","hot dog","hamburger","chicken","French","Fries",
+     "Coke"," juice","milk","water","tea","coffee","one","two","three","four",
+     "five","six","seven","eight","nine","ten","doll","boat","ball","kite",
+     "balloon","car","plane"
      ],
+     // 三年级英语下册74个
      "level2": [
-     "tree",
-     "car",
-     "yes",
-     "green"
+      "boy","girl","teacher","student","this","my","friend","I am","nice","good morning",
+      "good afternoon","meet","goodbye","too","father","dad","mother","mom","man","woman",
+      "grandmother","grandma","grandfather","grandpa","sister","brother","let us","great","really",
+      "and","how","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen",
+      "nineteen","twenty","how","many","can","look at","peach","pear","orange","watermelon",
+      "apple","banana","strawberry ","grape","like","some","thanks","bus","bike","taxi",
+      "jeep","desk","chair","Walkman","lamp","your","zoo","small","big","long",
+      "short","tall","giraffe","deer"
      ],
+    // 四年级英语上册
+    "level3": [
+     "window","board","light","picture","door","floor","classroom","many","our","classmate",
+     "seat","near","computer","teacher's desk","wall","fan","clean","Chinese","English","math",
+     "book","schoolbag","notebook","colour","fat","may","sure","here you are","bag",
+     "pencil","pen","twenty-one","thirty","thirty-one","forty","forty-one","fifty","heavy","what's","sorry",
+     "book","ruler","pencil-case","long","hair","short","hair","thin","strong","quiet","friends",
+     "Chinese","like","his","photo","he's","has","name","he","teacher","student",
+     "music","science","sports","computer","game","painting","she's","her","you're","she",
+     "right","boy","girl","friend","study","bathroom","bedroom","living room","kitchen","it's",
+     "hey","fish","isn't","here","home","room","school","classroom","phone","bed",
+     "sofa","shelf","fridge","table","they","aren't","they're","key","open","look",
+     "please","on","no","window","desk","door","chair","bed","rice","fish",
+     "noodle","beef","vegetable","soup","have","dinner","wait","I would like","bread","milk",
+     "egg","water","hungry","for","thank you","knife","chopsticks","spoon","plate","fork",
+     "help","pass","ready","try","help yourself","show","yummy","food","use",
+     "chicken","fish","family","parents","uncle","aunt","baby","people","member","only",
+     "puppy","gee","come","who","sister","brother","father","mother","baseball player",
+     "driver","doctor","farmer","nurse","look","young"
+    ],
      
 }
-let levelNameList = ["level1","level2"];
+let levelNameList = ["level1","level2","level3"];
 
  
