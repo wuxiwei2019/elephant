256ba6f9d2ea36be49a6005d744f8a6bc4055f05
WuXiWei <85207476@qq.com>
2021-04-26 10:20:59 +0800

    v1.2 add DeskTopShortcut

--- 大象英语.bat
+++ 大象英语.bat
@@ -1,4 +1,19 @@
 @echo off
-set current_path=%cd%
-::start chrome.exe --allow-file-access-from-files  --app=https://www.baidu.com
-start chrome.exe --allow-file-access-from-files  --app=file:///%current_path%/index.html
+set current_path=%cd%\index.html
+::start chrome.exe --allow-file-access-from-files  --app=https://www.baidu.com 
+
+:: create VBS
+::set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+ 
+::echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+::echo sLinkFile = "%USERPROFILE%\Desktop\Elephant.lnk" >> %SCRIPT%
+::echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+::echo oLink.TargetPath = "%current_path%" >> %SCRIPT%
+::echo oLink.TargetPath = "chrome.exe --allow-file-access-from-files  --app=file:///%current_path%" >> %SCRIPT%
+::echo oLink.Save >> %SCRIPT%
+ 
+::cscript /nologo %SCRIPT%
+::echo 创建快捷方式成功！
+::pause
+::del %SCRIPT%
+start chrome.exe --allow-file-access-from-files  --app=file:///%current_path%
